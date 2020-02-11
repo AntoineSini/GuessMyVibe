8acae5009c0cd0b0f871cf7cce4297c77a7fd153
Antoine <antoinesini@hotmail.fr>
2020-02-11 14:58:38 +0100

    Register Activity + layout custom (+ new font style and color)

--- app/src/main/java/fr/isen/guessmyvibe/LoginActivity.kt
+++ app/src/main/java/fr/isen/guessmyvibe/LoginActivity.kt
@@ -1,13 +1,55 @@
 package fr.isen.guessmyvibe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        
+        //Initialize
+        auth = FirebaseAuth.getInstance()
+        signupButton.setOnClickListener{
+            intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        signinButton.setOnClickListener{
+            signIn(emailTextInput.text.toString(), passwordTextInput.text.toString())
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            intent = Intent(this, HomeActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun signIn(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d("antoine", "signInWithEmail:success")
+                val user = auth.currentUser
+                intent = Intent(this, HomeActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w("antoine", "signInWithEmail:failure", task.exception)
+                Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
