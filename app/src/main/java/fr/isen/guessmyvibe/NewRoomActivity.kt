83cacbd5733d704b00e2403a18dba3b83f664a71
Morgane Gougeon <gougeon.morgane@gmail.com>
2020-02-11 16:45:48 +0100

    End of the listeners for buttons

--- app/src/main/java/fr/isen/guessmyvibe/NewRoomActivity.kt
+++ app/src/main/java/fr/isen/guessmyvibe/NewRoomActivity.kt
@@ -1,5 +1,6 @@
 package fr.isen.guessmyvibe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -11,5 +12,10 @@ class NewRoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_room)
+
+        playButton.setOnClickListener{
+            intent= Intent(this, MultiplayersGameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
