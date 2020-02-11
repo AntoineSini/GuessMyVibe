5fda26ec662c4df0e9e1524d0092fd116f7a4a15
Morgane Gougeon <gougeon.morgane@gmail.com>
2020-02-11 16:10:18 +0100

    Add listeners on buttons

--- app/src/main/java/fr/isen/guessmyvibe/HomeActivity.kt
+++ app/src/main/java/fr/isen/guessmyvibe/HomeActivity.kt
@@ -1,12 +1,35 @@
 package fr.isen.guessmyvibe
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        buttonsListener()
+    }
+
+    fun buttonsListener()
+    {
+        soloButton.setOnClickListener{
+            intent= Intent(this, LevelActivity::class.java)
+
+        }
+        multiplayersButton.setOnClickListener{
+            intent= Intent(this, MultiActivity::class.java)
+
+        }
+        profileButton.setOnClickListener{
+            intent= Intent(this, ProfileActivity::class.java)
+
+        }
+        startActivity(intent)
     }
 }
--- app/src/main/java/fr/isen/guessmyvibe/JoinRoomActivity.kt
+++ app/src/main/java/fr/isen/guessmyvibe/JoinRoomActivity.kt
@@ -1,12 +1,20 @@
 package fr.isen.guessmyvibe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_join_room.*
+
 
 class JoinRoomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_room)
+
+        joinButton.setOnClickListener{
+            intent= Intent(this, MultiplayersGameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
