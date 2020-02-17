68bb27250a0685f21fd028a4104d14ba345301a1
Morgane Gougeon <gougeon.morgane@gmail.com>
2020-02-17 11:10:18 +0100

    Pause a song OK

--- app/src/main/java/fr/isen/guessmyvibe/SpotifyActivity.kt
+++ app/src/main/java/fr/isen/guessmyvibe/SpotifyActivity.kt
@@ -1,11 +1,14 @@
 package fr.isen.guessmyvibe
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector.ConnectionListener
 import com.spotify.android.appremote.api.SpotifyAppRemote
+import kotlinx.android.synthetic.main.activity_spotify.*
 
 
 class SpotifyActivity : AppCompatActivity() {
@@ -13,10 +16,23 @@ class SpotifyActivity : AppCompatActivity() {
     private val CLIENT_ID = "53e8b8b1bb7244f1ac977da374705352"
     private val REDIRECT_URI = "https://www.google.fr"
     private var mSpotifyAppRemote: SpotifyAppRemote? = null
+    private var CONNECTED = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spotify)
+
+        playButton.setOnClickListener(){
+            if (CONNECTED == 1){
+                connected()
+            }
+        }
+        stopButton.setOnClickListener(){
+                Toast.makeText(this, "stop btn", Toast.LENGTH_LONG).show()
+                onStop()
+
+        }
+
     }
 
     override fun onStart() {
@@ -34,7 +50,7 @@ class SpotifyActivity : AppCompatActivity() {
                     mSpotifyAppRemote = spotifyAppRemote
                     Log.d("MainActivity", "Connected! Yay!")
                     // Now you can start interacting with App Remote
-                    connected()
+                    CONNECTED = 1
                 }
 
                 override fun onFailure(throwable: Throwable) {
@@ -45,11 +61,15 @@ class SpotifyActivity : AppCompatActivity() {
     }
 
     private fun connected() { // Then we will write some more code here.
-        mSpotifyAppRemote?.getPlayerApi()?.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL")
+        mSpotifyAppRemote?.getPlayerApi()?.play("spotify:playlist:37i9dQZF1DX4WpYT2fah9c")
     }
 
     override fun onStop() {
         super.onStop()
         // Aaand we will finish off here.
+        mSpotifyAppRemote?.getPlayerApi()?.pause()
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote)
+
+
     }
 }
